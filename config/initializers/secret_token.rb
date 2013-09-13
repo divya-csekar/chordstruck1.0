7683e0d69fefc8c32022b556a281fcf2174ea3e4
Divya Chandra Sekar <divya.chandra.sekar@sv.cmu.edu>
2013-09-13 04:20:54 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chordstruck1::Application.config.secret_token = '697b931f16410da037734299ae29348b13fcdfaba7c91bad96bc2c7e0cc2f7f33bec93fe5a85bc5a806eaec3273a2e117e9ead04174df370080b1bfc4b0bc2e2'
